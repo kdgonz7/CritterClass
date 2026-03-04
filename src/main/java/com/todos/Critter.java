82d1ad32fd3af231619363ec0d1b8d9d74a8056a
ALC PC 3 <kgonzalez10@student.cscc.edu>
2026-03-04 12:39:48 -0500

    Refactor Critter communication methods and add migration XML files

--- src/main/java/com/todos/Critter.java
+++ src/main/java/com/todos/Critter.java
@@ -52,15 +52,8 @@ public class Critter {
 
         System.out.println(name + " is playing with mood " + mood + " and boredom " + boredom + " and says....");
 
-        OllamaResult result =
-                ollama.generate(
-                        OllamaGenerateRequest.builder()
-                                .withModel(model)
-                                .withPrompt("You are a critter. Make one sentence displaying how you feel. Your mood is " + mood + " and your boredom is " + boredom + ".")
-                                .build(),
-                        null);
-
-        System.out.println(name + ": " + result.getResponse());
+        String resp = talk("You are a critter. Make one sentence displaying how you feel. Your mood is " + mood + " and your boredom is " + boredom + ". A higher mood number means you are happier, and a higher boredom number means you are more bored. Make sure to include the mood and boredom numbers in your response.");
+        System.out.println(name + ": " + resp);
 
         mood += 10;
         boredom -= 10;
@@ -72,23 +65,25 @@ public class Critter {
         }
     }
 
-    public void talkTo(Critter critter2) throws OllamaException {
+    public String talk(String prompt) throws OllamaException {
         Ollama ollama = new Ollama();
-        // We're just using our quick-setup utility here to instantiate Ollama. Use the following
-        // to set it up with your Ollama configuration.
-        // Ollama ollama = new Ollama("http://your-ollama-host:11434/");
-        String model = "gemma3:1b";
-        ollama.pullModel(model);
 
+        String model = "gemma3:4b";
+        ollama.pullModel(model);
 
         OllamaResult result =
                 ollama.generate(
                         OllamaGenerateRequest.builder()
                                 .withModel(model)
-                                .withPrompt("You are a critter. You are communicating with another critter, their name is " + critter2.getName() + ", their mood is " + critter2.getMood() + " and their boredom is " + critter2.getBoredom() + ". Your mood is " + mood + " and your boredom is " + boredom + ".")
+                                .withPrompt(prompt)
                                 .build(),
                         null);
 
-        System.out.println(name + " says to " + critter2.getName() + ": " + result.getResponse());
+        return result.getResponse();
+    }
+
+    public void talkTo(Critter critter2) throws OllamaException {
+        String convo = "You are a critter named " + name + ". You are talking to another critter named " + critter2.getName() + ". You say: Hello " + critter2.getName() + "! How are you?";
+        System.out.println(name + " says to " + critter2.getName() + ": " + convo);
     }
 }
